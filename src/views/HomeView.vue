c3a4bb9f64b4a3810503d7bf43210d65812ef1c7
Rick Schultz <rick-schultz@hotmail.com>
2023-02-11 14:57:09 +0100

    added sasss and removed unnecessary default files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>This is a home page</h1>
+  </div>
 </template>
+
+
+<style lang="scss" scoped>
+
+</style>
