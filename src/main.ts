d01e70ea40ed3dd24d6d0990678788450818e019
Rick Schultz <rick-schultz@hotmail.com>
2023-02-11 16:11:57 +0100

    added main scss and added axios to main

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import axios from 'axios'
 import router from './router'
+import VueAxios from 'vue-axios'
+
+import './assets/main.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueAxios, axios)
 
 app.mount('#app')
