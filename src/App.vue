592bd51184f8504d16c524d242e8124d3933715c
Rick Schultz <rick-schultz@hotmail.com>
2023-02-11 16:12:25 +0100

    created NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NavBar />
   </header>
 
   <RouterView />
---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <nav>
+      <RouterLink to="/"><h1>Pokemon Search</h1></RouterLink>
+      <div>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #333;
+  color: #fff;
+  
+  nav {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+
+    div {
+      display: flex;
+      gap: 2rem;
+    }
+  }
+}
+</style>
