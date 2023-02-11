7cd64fe8ad73bba316194f6a3736c9092ca3fc59
Rick Schultz <rick-schultz@hotmail.com>
2023-02-11 16:12:45 +0100

    added simple store

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,29 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+const URL = 'https://pokeapi.co/api/v2/'
+
+export const useCharacterStore = defineStore('character', () => {
+
+  const characters = ref([])
+
+  async function getCharacters() {
+    const pokemons = await axios.get(URL + 'pokemon?limit=20&offset=0')
+    characters.value = pokemons.data.results
   }
 
-  return { count, doubleCount, increment }
+  async function getCharacterById(id: number) {
+    const pokemon = await axios.get(URL + 'pokemon/' + id)
+    console.log(pokemon.data)
+    return pokemon.data
+  }
+  
+  async function getCharacterByName(name: string) {
+    const pokemon = await axios.get(URL + 'pokemon/' + name)
+    console.log(pokemon.data)
+    return pokemon.data
+  }
+
+  return { characters, getCharacters, getCharacterById, getCharacterByName }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,43 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useCharacterStore } from '@/stores/counter';
+
+const store = useCharacterStore()
+
+store.getCharacters()
+store.getCharacterById(132)
+store.getCharacterByName('ditto')
+
+const clearValue = () => {
+  input.value = ''
+}
+
+const input = ref('Something')
 
 </script>
 
 <template>
-  <div>
-    <h1>This is a home page</h1>
+  <div class="main">
+    <input type="text" id="searchBar" v-model="input" placeholder="Search for a character">
+    <button @click="clearValue()">Clear</button>
+  </div>
+  <div
+  v-for="character in store.characters"
+  :key="character.name"
+  >
+  <p>{{ character.name }}</p>
+  <a>{{ character.url }}</a>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
-
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 2rem;
+}
 </style>
